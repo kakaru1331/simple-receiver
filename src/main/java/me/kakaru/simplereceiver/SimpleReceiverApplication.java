92e7af55c8c5da2dc0e09a1252e564d4c3284d65
kakaru1331 <kakaru1331@gmail.com>
2022-09-26 17:49:58 +0900

    init

---
+++ src/main/java/me/kakaru/simplereceiver/SimpleReceiverApplication.java
@@ -0,0 +1,13 @@
+package me.kakaru.simplereceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleReceiverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kakaru/simplereceiver/SimpleReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kakaru.simplereceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleReceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
